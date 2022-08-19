c587b5c094b9716597e19d3300d5893a65275ad8
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-08-18 23:54:08 -0300

    :tada: feat: Modelo cru de header responsivel

--- Client/src/components/Header/index.tsx
+++ Client/src/components/Header/index.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { FiLogOut } from 'react-icons/fi'
+import { FcManager } from 'react-icons/fc'
+import { FaBars } from 'react-icons/fa'
 import './styles.css';
+import { convertFirstToUpperCase } from '../../utils'
 
 export default function Header() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
   return (
-    <div>Header aqui lourenzetti</div>
-  )
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        Modelo header
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
+        <FaBars />
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="/contato">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
---
+++ Client/src/utils/index.tsx
@@ -0,0 +1,5 @@
+export function convertFirstToUpperCase(text: string){
+    const result = text[0].toUpperCase() + text.substring(1);
+    
+    return result
+  }
