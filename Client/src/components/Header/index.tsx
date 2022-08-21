843f972dffbe14d310a2ec0c2842ddc1351c7117
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-08-20 21:03:37 -0300

    :dizzy: Add animações na navbar
    Mudanças:
    - Pequenas animações no hover das navbar do header
    - Add um svg do react-icon para demonstrar o início na navbar

--- Client/src/components/Header/index.tsx
+++ Client/src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { FiLogOut } from 'react-icons/fi'
 import { IoLanguage } from 'react-icons/io5'
+import { AiOutlineHome } from 'react-icons/ai'
 import { FaBars } from 'react-icons/fa'
 import './styles.css';
 import { convertFirstToUpperCase } from '../../utils'
@@ -13,18 +14,18 @@ export default function Header() {
     <nav className="navigation">
       <div className='grid grid-cols-3 justify-center'>
         <div className='flex'>
-        <div className=" menu-language-dropdown">
-            <span>Language</span>
-            <div className="language-dropdown-content">
-            <ul>
-              <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\brazil.png" /></a></li>
-              <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\france.png" /></a></li>
-              <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\spain.png" /></a></li>
-              <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\usa.png" /></a></li>
-            </ul>
+          <div className=" menu-language-dropdown">
+              <span>Language</span> 
+              <div className="language-dropdown-content">
+              <ul>
+                <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\brazil.png" /></a></li>
+                <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\france.png" /></a></li>
+                <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\spain.png" /></a></li>
+                <li><a href="#"><img className='w-auto h-10 m-1 ml-10' src="src\assets\languages\usa.png" /></a></li>
+              </ul>
+              </div>
             </div>
           </div>
-        </div>
         <div className='flex justify-center'>
           <img className='w-auto h-20' src="src\assets\logo-banner.png" />
         </div>
@@ -44,7 +45,7 @@ export default function Header() {
       >
         <ul className='navigation-menu-ul'>
           <li>
-            <a href="/">Inicio</a>
+            <a href="/"> <AiOutlineHome size={20}/>Início </a>
           </li>
           <li>
             <a href="/sobre">Sobre</a>
