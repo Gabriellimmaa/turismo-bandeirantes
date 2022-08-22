c6fe8aa9674466876849c0e75e05ce5f748412b6
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-08-22 00:53:10 -0300

    :construction: Texto pagina historia

--- Client/src/screens/Historia/index.tsx
+++ Client/src/screens/Historia/index.tsx
@@ -2,9 +2,60 @@ import React from 'react'
 
 export default function Historia() {
   return (
-    <div>
-      <h1>Historia</h1>
-      <p>src\screens\Historia\index.tsx</p>
-    </div>
+    <div className='px-32 py-16'>
+      <h1 className=''>Historia</h1>
+      <p>Uma série de atrações aguarda turistas e brasilienses para descobrirem o que a capital do país tem de melhor. A concepção da cidade, sítio moderno considerado Patrimônio Cultural da Humanidade pela Unesco, convida a um passeio surpreendente.</p>
+
+      <p>Prédios homogêneos, ruas largas e arborizadas, parques e setores pensados no bem-estar da população e do visitante levam a assinatura dos mestres do urbanismo Lucio Costa e da arquitetura Oscar Niemeyer.</p>
+      <h1>Roteiro Cultural
+
+      </h1>
+      <p>Os monumentos de Brasília são únicos e dispostos de maneira a formar um museu a céu aberto. No interior dos prédios, obras de artistas renomados podem ser visitadas, em horários específicos, sem nenhum custo.</p>
+      <p>No Palácio do Itamaraty, Sérgio Camargo, Rubem Valentim, Alfredo Volpi e Bruno Giorgi assinam telas e esculturas famosas. A poucos metros, no Palácio do Planalto (sede da Presidência da República), algumas peças foram criadas exclusivamente para o local, como a tapeçaria Músicos, de Di Cavalcanti, que recebeu a incumbência do arquiteto Oscar Niemeyer, autor do projeto do prédio. O Planalto possui, também, um rico mobiliário, porcelanas da Companhia das Índias e prataria portuguesa do século XVIII.</p>
+      <p>Assim como o Executivo, as sedes do Legislativo e do Judiciário também abrem as portas para o cidadão. No Congresso Nacional, há o projeto Visite o Senado. A versão do Supremo Tribunal Federal se chama Portas Abertas do STF e recebe cerca de 400 visitantes por dia. A oportunidade é para conhecer de perto como funcionam os prédios onde são tomadas as mais importantes decisões do país. Do lado de fora, a Praça dos Três Poderes proporciona uma visão privilegiada das instalações da República.</p>
+      <p>Acima da Esplanada dos Ministérios, a Catedral Metropolitana de Brasília, a Torre de TV, o Memorial JK e o Complexo Cultural da República completam a arquitetura histórica e cultural da região central de Brasília.</p>
+      <h1>Embaixador de Brasília</h1>
+      <p>Oscar Niemeyer projetou, ao longo de seus 104 anos, inúmeras obras em 18 países. Cerca de 80 edificações no Distrito Federal trazem sua assinatura – a primeira delas antes mesmo da inauguração de Brasília: o Catetinho, erguido em apenas 10 dias, em 1955. A mais recente foi a Torre de TV Digital, entregue ao público no dia 21 de abril, na comemoração dos 52 anos de fundação da cidade.</p>
+      <p>Entre as obras da capital destacam-se os palácios da Alvorada, do Planalto, do Itamaraty e da Justiça; os edifícios-sede do Congresso Nacional, Supremo Tribunal Federal, Superior Tribunal de Justiça e tribunais superiores Eleitoral e do Trabalho. O arquiteto projetou a Catedral e os prédios do Conjunto Cultural da República, da Esplanada dos Ministérios, do Ministério Público da União, da Ordem dos Advogados do Brasil e do Instituto de Arquitetos do Brasil.</p>
+      <p>Além das sedes dos três Poderes federais, são de sua autoria os projetos arquitetônicos do Teatro Nacional de Brasília, do Cine Brasília e da Casa do Cantador. O arquiteto ainda assina o Panteão da Liberdade e da Democracia, o Centro Cultural de Taguatinga, a Ponte Costa e Silva, alguns prédios da Universidade de Brasília, o Memorial JK e o Aeroporto Internacional de Brasília.</p>
+      <h1>Brasília em números
+
+      </h1>
+      <p>Inauguração: 21 de abril de 1960
+
+        Gentílico: Brasiliense
+
+        População [2010]: 2.570.160 – 53,72% nascida no DF.Entre os migrantes, 51,4% são da região nordeste do país
+
+        Área da unidade territorial [2016]: 5.779,997 km²
+
+        Área: 5.780 km² – 96,6% urbana e 3,4% rural
+
+        Densidade demográfica: 354,3 hab/km²
+
+        Bioma: Cerrado
+
+        Altitude: 1.172 m
+
+        Clima: Tropical, continental de altitude, com duas estações bem marcadas: uma chuvosa (de novembro a março); e outra sem chuvas – nesta, a umidade relativa do ar costuma ser muito baixa.
+
+        Temperatura média: 18°C a 25°C.
+
+        Umidade média relativa do ar: 40 a 70%
+
+        Salário médio mensal dos trabalhadores formais [2015]: 5,7 salários mínimos
+
+        Percentual da população com rendimento nominal mensal per capita de até ½ salário mínimo [2010]: 30,9 %
+
+        Renda per capita (média mensal): R$ 2.654,00 (IBGE/PNAD 2012)
+
+        Índice de Desenvolvimento Humano (IDH): 0,792 (segundo melhor índice do país)
+
+        Produto Interno Bruto (PIB): R$ 99,9 bilhões
+
+        PIB per capita [2015] 73.971,05 R$
+
+        Cidade mais populosa: Ceilândia, com 398.374 habitantes</p>
+    </div >
   )
 }
