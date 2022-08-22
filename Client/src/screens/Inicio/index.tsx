6cb65933f70a61be6982898644d1c2c25663add9
rodrigo_juniorj <rodrigo.franco@unifio.edu.br>
2022-08-22 13:00:01 -0300

    :bug: fix: Error carrocel

--- Client/src/screens/Inicio/index.tsx
+++ Client/src/screens/Inicio/index.tsx
@@ -1,11 +1,31 @@
 import { Carousel } from '../../components/Carousel'
+import { GiSoccerBall } from 'react-icons/gi'
 
 export default function Inicio() {
   return (
-    <div className="w-full overflow-hidden h-full">
+    <>
       <div className="w-full h-[65%]">
         <Carousel />
       </div>
-    </div>
+      <div className="w-full mx-6 h-full mb-10">
+        <div className="w-full mt-10 ">
+          <h1 className="flex justify-center my-16 text-4xl font-extrabold uppercase">
+            Acesso rápido
+          </h1>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <h2>
+              <GiSoccerBall />
+            </h2>
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Lazer e esporte</div>
+              <p className="text-gray-700 text-base">
+                Nossa cidade é o seu destino perfeito Cultura, esporte, lazer,
+                muito ar puro e belos momentos natureza
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
