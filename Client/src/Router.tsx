bc8e29631a24352a850f1727ba82d903127a0235
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-08-22 19:30:37 -0300

    :sparkles:	feat Pagina turismo
     - Add a tela do turismo

--- Client/src/Router.tsx
+++ Client/src/Router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Historia from './screens/Historia'
 import Inicio from './screens/Inicio'
 import Restaurantes from './screens/Restaurantes'
+import { Turismo } from './screens/Turismo'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
       <Route path="/historia" element={<Historia />} />
+      <Route path="/turismo" element={<Turismo />} />
       <Route path="/restaurantes" element={<Restaurantes />} />
     </Routes>
   )
---
+++ Client/src/screens/Turismo/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { BiMap } from 'react-icons/bi'
+import { BiTime } from 'react-icons/bi'
+import './style.css'
+
+
+export function Turismo() {
+    return(
+        <div className='turismo'>
+            <h1>
+                Turismo
+            </h1>
+            <div className='main-options'>
+                <ul>
+                    <li>Religioso</li>
+                    <li>Rural</li>
+                    <li>Cultural</li>
+                </ul>
+            </div>
+            <div className='grid grid-cols-3 gap-2'>
+                <div className='card'>
+                    <div className='card-img'>
+                        <img src="src\assets\img\turismo\01_512x342.jpg" alt="Santuário São Miguel" />
+                    </div>
+                    <div className='card-text'>
+                        <label>Título</label>
+                        <p> <BiTime/> Horário de visitação: 7:00 - 22:00</p>
+                        <p>Breve descrição</p>
+                        <p> <BiMap/> Localização</p>
+                        <h4>Saiba mais</h4>
+                    </div>
+                </div>
+                
+                <div className='card'>
+                    <div className='card-img'>
+                        <img src="src\assets\img\turismo\01_512x342.jpg" alt="Santuário São Miguel" />
+                    </div>
+                    <div className='card-text'>
+                        <label>Título</label>
+                        <p> <BiTime/> Horário de visitação: 7:00 - 22:00</p>
+                        <p>Breve descrição</p>
+                        <p> <BiMap/> Localização</p>
+                        <h4>Saiba mais</h4>
+                    </div>
+                </div>
+
+                <div className='card'>
+                    <div className='card-img'>
+                        <img src="src\assets\img\turismo\01_512x342.jpg" alt="Santuário São Miguel" />
+                    </div>
+                    <div className='card-text'>
+                        <label>Título</label>
+                        <p> <BiTime/> Horário de visitação: 7:00 - 22:00</p>
+                        <p>Breve descrição</p>
+                        <p> <BiMap/> Localização</p>
+                        <h4>Saiba mais</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
