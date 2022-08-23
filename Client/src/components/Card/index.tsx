f647ef22efb5512147c9edda26d8f8cba3367226
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-08-22 23:11:36 -0300

    :lipstick: style: Card components mobile

--- Client/src/components/Card/index.tsx
+++ Client/src/components/Card/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import { BiMap } from 'react-icons/bi'
 import { BiTime } from 'react-icons/bi'
+import { limitDescription } from '../../utils';
 import './styles.css'
 
 interface CardProps {
@@ -11,27 +11,28 @@ interface CardProps {
     redirectDetail: string;
 }
 
-export default function Card({title, description, date, redirectMap, redirectDetail}: CardProps) {
+export default function Card({ title, description, date, redirectMap, redirectDetail }: CardProps) {
     return (
         <div className="card">
             <div className="card-img">
                 <img src="src\assets\img\restaurante\01.jpg" />
             </div>
-            <div className="card-text">
-                <label>{title}</label>
-                <div className='description'>
-                    <p>{description}</p>
-                </div>
-                <div className='info grid grid-cols-2'>
-                    <div>
-                        <BiTime size={16} className="mr-1" /><p>{date}</p>
-                    </div>
-                    <div>
-                        <BiMap size={16} className="mr-1" /><a href={redirectMap} target="_blank">Abrir com google maps</a>
-                    </div>
-                </div>
-                <a className='redirect' href={redirectDetail}>Ver mais</a>
+            <label className='title margin'>{title}</label>
+            <div className='description margin'>
+                <p>{limitDescription(description)}</p>
             </div>
+            <div className='info margin grid grid-cols-2'>
+                <div>
+                    <BiTime size={16} className="mr-1" /><p>{date}</p>
+                </div>
+                <div>
+                    <BiMap size={16} className="mr-1" /><a href={redirectMap} target="_blank">Abrir com google maps</a>
+                </div>
+            </div>
+            <div className='redirect'>
+                <a className='margin' href={redirectDetail}>Ver mais</a>
+            </div>
+
         </div>
     )
 }
