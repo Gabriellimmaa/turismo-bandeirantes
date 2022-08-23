241708f696b4659c18dd941ab8c2a15dfa9e8614
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-08-22 23:11:53 -0300

    :sparkles: feat: Function limiteDescription

--- Client/src/utils/index.tsx
+++ Client/src/utils/index.tsx
@@ -3,3 +3,14 @@ export function convertFirstToUpperCase(text: string) {
 
   return result
 }
+
+
+export function limitDescription(description: string) {
+  if (window.innerWidth >= 2000) {
+    return description.substring(0, 400) + "...";
+  } else if (window.innerWidth <= 1024) {
+    return description.substring(0, 50) + "...";
+  }else {
+    return description.substring(0, 160) + "...";
+  }
+}
