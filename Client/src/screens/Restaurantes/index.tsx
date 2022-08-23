8a8f92f83cd53fcb59eef101d7302ddce08bd8ab
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-08-22 23:12:37 -0300

    :lipstick: style: Restaurante card mobile

--- Client/src/screens/Restaurantes/index.tsx
+++ Client/src/screens/Restaurantes/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Card from '../../components/Card'
+import {MdFastfood} from 'react-icons/md'
 import './styles.css'
 
 export default function Restaurantes() {
   return (
-    <div id="restaurante" className='px-32 py-16'>
-      <h1>
-        Restaurantes
+    <div id="restaurante" >
+      <h1 className='text-center w-full text-5xl'>
+        RESTAURANTES
       </h1>
       <div className='option'>
         <ul className='bg-gray-200'>
@@ -17,8 +18,8 @@ export default function Restaurantes() {
           <li>Padaria</li>
         </ul>
       </div>
-      <div className="grid grid-cols-3 gap-2 justify-items-center">
-        <Card title='Bom de garfo' description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic qui tenetur similique perferendis dolores enim corrupti, inventore commodi a, consequatur accusamus iusto rerum iure delectus, alias sit maiores itaque odit.' date='07:00 às 19:00' redirectDetail='https://xvideos.com' redirectMap='https://xvideos.com' />
+      <div className="grid grid-cols-3 gap-5 justify-items-center">
+        <Card title='Bom de garfo' description='asdasd' date='07:00 às 19:00' redirectDetail='https://xvideos.com' redirectMap='https://xvideos.com' />
         <Card title='Bom de garfo' description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic qui tenetur similique perferendis dolores enim corrupti, inventore commodi a, consequatur accusamus iusto rerum iure delectus, alias sit maiores itaque odit.' date='07:00 às 19:00' redirectDetail='https://xvideos.com' redirectMap='https://xvideos.com' />
         <Card title='Bom de garfo' description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic qui tenetur similique perferendis dolores enim corrupti, inventore commodi a, consequatur accusamus iusto rerum iure delectus, alias sit maiores itaque odit.' date='07:00 às 19:00' redirectDetail='https://xvideos.com' redirectMap='https://xvideos.com' />
         <Card title='Bom de garfo' description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic qui tenetur similique perferendis dolores enim corrupti, inventore commodi a, consequatur accusamus iusto rerum iure delectus, alias sit maiores itaque odit.' date='07:00 às 19:00' redirectDetail='https://xvideos.com' redirectMap='https://xvideos.com' />
