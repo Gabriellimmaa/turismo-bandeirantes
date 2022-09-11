73498e17d38448a2b240233e53c026a2f448e1e6
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-11 09:57:06 -0300

    :construction: feat: Modelo de geolocalização

---
+++ Client/src/hooks/usePosition.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+export const usePosition = () => {
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const onChange = ( coords: any) => {
+    setPosition({
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    });
+  };
+  const onError = (error: any) => {
+    toast.error("Geolocalização desativada, por favor ative para melhorar sua experiência");
+  };
+  useEffect(() => {
+    const geo = navigator.geolocation;
+    if (!geo) {
+      toast.error("Geolocalização não é suportada por este navegador");
+      return;
+    }
+    const watcher = geo.watchPosition(onChange, onError);
+    return () => geo.clearWatch(watcher);
+  }, []);
+  return { ...position, error };
+}
