c4911fece2998e053f5a3d3a684ca07c361838ff
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-11 09:58:09 -0300

    :sparkles: feat: react-toastify

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,9 +3,21 @@ import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
 import { Footer } from './components/Footer'
 import './i18next';
+import { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+// minified version is also included
+// import 'react-toastify/dist/ReactToastify.min.css';
+
+import { usePosition } from './hooks/usePosition'
+
 function App() {
+  usePosition();
+  
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Header />
       <Router />
       <Footer />
