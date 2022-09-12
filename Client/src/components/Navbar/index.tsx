eb57e2b9ada812a5a041a4d8032f947eb5afcf41
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-12 19:57:38 -0300

    Update NavBar

--- Client/src/components/Navbar/index.tsx
+++ Client/src/components/Navbar/index.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 interface ObjectItem {
   id: string
   nome: string
-  categoria: string
-  type: string
+  categoria?: string
+  type?: string
 }
 
 interface NavbarProps {
@@ -17,10 +17,15 @@ export default function Navbar({ objectList }: NavbarProps) {
     <div className="navbar">
       <ul>
         {objectList.map((data: ObjectItem) => (
-          <li key={data.id}>
-            <Link to={`/${data.type}/${data.categoria}`}><button>{data.nome}</button></Link>
-          </li>
-        ))}
+          data.categoria ? (
+            <li key={data.id}>
+              <Link to={`/${data.type}/${data.categoria}`}><button>{data.nome}</button></Link>
+            </li>
+          ) : (
+            <li key={data.id}>
+              <Link to={`/${data.nome}`}><button>{data.nome}</button></Link>
+            </li>
+        )))}
       </ul>
     </div>
   )
