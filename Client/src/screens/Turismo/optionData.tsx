91768cc7a189d3d765a64b8ee7871256f8f63e70
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-12 19:57:08 -0300

    :lipstick: style: TurismoDetail

--- Client/src/screens/Turismo/optionData.tsx
+++ Client/src/screens/Turismo/optionData.tsx
@@ -1,20 +1,20 @@
 export const optionTurismo = [
   {
-    "id": "01",
-    "nome": "Turismo religioso",
-    "categoria": "religioso",
-    "type": "turismo"
+    id: "01",
+    nome: "Turismo religioso",
+    categoria: "religioso",
+    type: "turismo"
 },
   {
-    "id": "02",
-    "nome": "Turismo Cultural",
-    "categoria": "cultural",
-    "type": "turismo"
+    id: "02",
+    nome: "Turismo Cultural",
+    categoria: "cultural",
+    type: "turismo"
   },
   {
-    "id": "03",
-    "nome": "Turismo Rural",
-    "categoria": "rural",
-    "type": "turismo"
+    id: "03",
+    nome: "Turismo Rural",
+    categoria: "rural",
+    type: "turismo"
   }
 ]
