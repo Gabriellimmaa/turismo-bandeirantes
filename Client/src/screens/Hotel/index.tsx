9c123f24cd9a8146ef9e0ed28269f3b36e5b018f
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-09-12 20:13:26 -0300

    :lipstick: feat: Arrumando bug da página de hotel

--- Client/src/screens/Hotel/index.tsx
+++ Client/src/screens/Hotel/index.tsx
@@ -49,7 +49,7 @@ export function Hoteis() {
                   img={hotel.logo}
                   title={hotel.nome}
                   price={hotel.preco}
-                  type="hoteis"
+                  type="hotel"
                   cell={hotel.telefone}
                   email={hotel.email}
                   website={hotel.site}
