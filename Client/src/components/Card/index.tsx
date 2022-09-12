9d1a6319604975f32c28aedb722a15f6428cc6cc
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-09-12 13:02:00 -0300

    :lipstick: feat: Padronizando animação dos cards

--- Client/src/components/Card/index.tsx
+++ Client/src/components/Card/index.tsx
@@ -1,11 +1,18 @@
-import { BiCalendar, BiMailSend, BiMap, BiMoney, BiPhoneCall, BiPlanet, BiTime } from 'react-icons/bi'
+import {
+  BiCalendar,
+  BiMap,
+  BiMoney,
+  BiPhoneCall,
+  BiPlanet,
+  BiTime,
+} from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 import { limitDescription } from '../../utils'
 import './styles.css'
 
 interface CardProps {
   title: string
-  type: "hotel" | "restaurante" | "turismo" | "lazer&esporte" | "agenda"
+  type: 'hotel' | 'restaurante' | 'turismo' | 'lazer&esporte' | 'agenda'
   img: string
   id: number
   description?: string
@@ -37,7 +44,7 @@ export default function Card({
   id,
 }: CardProps) {
   return (
-    <div className='card'>
+    <div className="card">
       <div className="card-container">
         <Link className="card-img" to={`/${type}/${id}`}>
           <img src={img} />
@@ -45,57 +52,67 @@ export default function Card({
         <label className="title margin">{title}</label>
         <div className="description">
           {description ? <p>{limitDescription(description)}</p> : null}
-          {email ?
-            <div className='flex items-center'>
+          {email ? (
+            <div className="flex items-center">
               {/* <BiMailSend size={16} className="mr-1" /> */}
-              <p className='m-0'>{email}</p>
+              <p className="m-0">{email}</p>
             </div>
-            : null}
-          {locale ?
+          ) : null}
+          {locale ? (
             <div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <BiMap size={16} className="mr-1" />
                 <b>Local do evento:</b>
               </div>
               <p>{locale}</p>
             </div>
-            : null}
+          ) : null}
         </div>
         <div className="info margin grid grid-cols-2">
-          {website ?
-            <div className='website'>
+          {website ? (
+            <div className="website">
               <BiPlanet size={16} className="mr-1" />
               <a href={website}>Visite nosso site</a>
             </div>
-            : null}
-          {price ?
+          ) : null}
+          {price ? (
             <div>
               <BiMoney size={16} className="mr-1" />
-              <p><b>R$ {price}</b></p>
+              <p>
+                <b>R$ {price}</b>
+              </p>
             </div>
-            : null}
-          {date ?
+          ) : null}
+          {date ? (
             <div>
               <BiCalendar size={16} className="mr-1" />
-              <p><b>{date}</b></p>
+              <p>
+                <b>{date}</b>
+              </p>
             </div>
-            : null}
-          {hour ?
+          ) : null}
+          {hour ? (
             <div>
               <BiTime size={16} className="mr-1" />
-              <p><b>{hour}</b></p>
+              <p>
+                <b>{hour}</b>
+              </p>
             </div>
-            : null}
-          {cell ?
+          ) : null}
+          {cell ? (
             <div>
               <BiPhoneCall size={16} className="mr-1" />
-              <p><b>{cell}</b></p>
+              <p>
+                <b>{cell}</b>
+              </p>
             </div>
-            : null}
+          ) : null}
         </div>
       </div>
       <div className="redirect">
-        <Link id="cardMore" className="margin" to={`/${type}/${id}`}>Ver mais</Link>
+        <Link id="cardMore" className="margin" to={`/${type}/${id}`}>
+          Ver mais
+        </Link>
         {latitude && longitude && (
           <a
             id="cardMap"
@@ -108,6 +125,5 @@ export default function Card({
         )}
       </div>
     </div>
-
   )
 }
