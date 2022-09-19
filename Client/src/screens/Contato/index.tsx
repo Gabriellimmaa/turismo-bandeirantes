153dd8f36cdea2f215960216103cba6768559c5b
Carlos Alberto Andrino Junior <carlos.andrinojr@gmail.com>
2022-09-19 20:50:04 -0300

    :lipstick: feat: Reestilizando página de contatos

--- Client/src/screens/Contato/index.tsx
+++ Client/src/screens/Contato/index.tsx
@@ -7,32 +7,36 @@ export default function Contato() {
     <section className="contato">
       <h1>Entre em contato conosco</h1>
       <div className="pagina">
-        <div className="left">
-          <div className="Endereço ">
-            <h2> Endereço {<BsFillPinMapFill size={30} />}</h2>
-            <iframe
-              className="mapa"
-              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEM-BKN-O6irIoDU8C-G9OFtPUvgb6bjg&q=-23°06'22.9, -50°22'18.7"
-            ></iframe>
-          </div>
-
-          <div className="telefone">
-            <h2>Telefone {<AiFillPhone size={30} />}</h2>
-
-            <p>(43) 3542-4525</p>
-          </div>
+        <div className="cartao">
+        <h3>Endereço</h3>
+        <iframe className="mapa"src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEM-BKN-O6irIoDU8C-G9OFtPUvgb6bjg&q=-23°06'22.9, -50°22'18.7"></iframe>
+          <h3>Telefone</h3>
+          <p className="title">(43)3542-4525</p>
+          <p>Prefeitura Municipal de Bandeirantes</p>
+          <p><button>Entrar em contato</button></p>
         </div>
         <div className="rigth">
           <div className="entreemcontato">
-            <h2>Envie-nos uma Mensagem {<AiFillMessage size={30} />}</h2>
-            <p>Nome:</p>
-            <textarea className="caixatexto"></textarea>
-            <p>E-mail:</p>
-            <textarea className="caixatexto"></textarea>
-            <p>Contato:</p>
-            <textarea className="caixatexto"></textarea>
-            <p>Sugestão:</p>
-            <textarea className="caixatexto1"></textarea>
+            <h3>Envie-nos uma Mensagem </h3>
+            <form action="action_page.php">
+              <label htmlFor="nome">Nome:</label>
+              <input type="text" id="nome" name="nome" placeholder="Seu nome..." />
+              <label htmlFor="telefone">Telefone:</label>
+              <input type="text" id="Telefone" name="telefone" placeholder="Seu telefone..." />
+              <label htmlFor="email">E-Mail:</label>
+              <input type="text" id="email" name="email" placeholder="Seu e-mail..." />
+              <label htmlFor="tsugest">Tipo da sugestão:</label>
+              <select id="tsugest" name="tsugest">
+                <option value="turismo">Turismo</option>
+                <option value="restaurante">Restaurante</option>
+                <option value="hotéis">Hotéis</option>
+                <option value="agenda">Agenda</option>
+                <option value="outros">Outros...</option>
+              </select>
+              <label htmlFor="sugestão">Sugestão:</label>
+              <textarea id="sugestão" name="sugestão" placeholder="Escreva aqui sua sujestão..." style={{ height: '200px' }} defaultValue={""} />
+              <input type="enviar" defaultValue="Enviar" />
+            </form>
           </div>
         </div>
       </div>
