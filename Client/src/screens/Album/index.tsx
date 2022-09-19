9e118ed2af49a82a86105ba44e13084db8b9d66b
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-09-19 14:51:43 -0300

    :lipstick: feat add img no album

--- Client/src/screens/Album/index.tsx
+++ Client/src/screens/Album/index.tsx
@@ -3,13 +3,25 @@ import Toolbar from '../../components/Toolbar'
 import './styles.css'
 
 export function Album() {
-  return (
-    <section id="album">
-      <div className="title-style-1">
-        <h1>Álbum</h1>
-        {/* <h2>Algumas fotos desta cidade encanto</h2> */}
-      </div>
-      
-    </section>
-  )
+    return (
+        <section id="album">
+            <div className="title-style-1">
+                <h1>Álbum</h1>
+                <h2>Veja as maravilhas da nossa cidade!</h2>
+                <div className='album-display'>
+                    <div className='album-content'>
+                        <img src="src\assets\img\album\album01.jpeg" alt="" />
+                        <img src="src\assets\img\album\album02.jpeg" alt="" />
+                        <img src="src\assets\img\album\album03.jpeg" alt="" />
+                        <img src="src\assets\img\album\album04.jpeg" alt="" />
+                        <img src="src\assets\img\album\album05.jpeg" alt="" />
+                        <img src="src\assets\img\album\album06.jpg" alt="" />
+                        <img src="src\assets\img\album\album07.jpg" alt="" />
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+    )
 }
