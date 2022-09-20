78803659ddfa191f1afba6d3d5eaf06552eb6f04
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-09-19 21:16:51 -0300

    :lipstick: feat: Ajustando efeito de hover

--- Client/src/screens/Contato/index.tsx
+++ Client/src/screens/Contato/index.tsx
@@ -1,6 +1,4 @@
 import './styles.css'
-import { AiFillPhone, AiFillMessage } from 'react-icons/ai'
-import { BsFillPinMapFill } from 'react-icons/bs'
 
 export default function Contato() {
   return (
@@ -8,23 +6,45 @@ export default function Contato() {
       <h1>Entre em contato conosco</h1>
       <div className="pagina">
         <div className="cartao">
-        <h3>Endereço</h3>
-        <iframe className="mapa"src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEM-BKN-O6irIoDU8C-G9OFtPUvgb6bjg&q=-23°06'22.9, -50°22'18.7"></iframe>
+          <h3>Endereço</h3>
+          <iframe
+            className="mapa"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEM-BKN-O6irIoDU8C-G9OFtPUvgb6bjg&q=-23°06'22.9, -50°22'18.7"
+          ></iframe>
           <h3>Telefone</h3>
           <p className="title">(43)3542-4525</p>
           <p>Prefeitura Municipal de Bandeirantes</p>
-          <p><button>Entrar em contato</button></p>
+          <p>
+            <button className="hover:bg-gray-700 transition-colors hover:transition-colors">
+              Entrar em contato
+            </button>
+          </p>
         </div>
         <div className="rigth">
           <div className="entreemcontato">
             <h3>Envie-nos uma Mensagem </h3>
             <form action="action_page.php">
               <label htmlFor="nome">Nome:</label>
-              <input type="text" id="nome" name="nome" placeholder="Seu nome..." />
+              <input
+                type="text"
+                id="nome"
+                name="nome"
+                placeholder="Seu nome..."
+              />
               <label htmlFor="telefone">Telefone:</label>
-              <input type="text" id="Telefone" name="telefone" placeholder="Seu telefone..." />
+              <input
+                type="text"
+                id="Telefone"
+                name="telefone"
+                placeholder="Seu telefone..."
+              />
               <label htmlFor="email">E-Mail:</label>
-              <input type="text" id="email" name="email" placeholder="Seu e-mail..." />
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Seu e-mail..."
+              />
               <label htmlFor="tsugest">Tipo da sugestão:</label>
               <select id="tsugest" name="tsugest">
                 <option value="turismo">Turismo</option>
@@ -34,8 +54,18 @@ export default function Contato() {
                 <option value="outros">Outros...</option>
               </select>
               <label htmlFor="sugestão">Sugestão:</label>
-              <textarea id="sugestão" name="sugestão" placeholder="Escreva aqui sua sujestão..." style={{ height: '200px' }} defaultValue={""} />
-              <input type="enviar" defaultValue="Enviar" />
+              <textarea
+                id="sugestão"
+                name="sugestão"
+                placeholder="Escreva aqui sua sujestão..."
+                style={{ height: '200px' }}
+                defaultValue={''}
+              />
+              <input
+                type="enviar"
+                defaultValue="Enviar"
+                className="hover:bg-gray-700 transition-colors hover:transition-colors"
+              />
             </form>
           </div>
         </div>
