4f6291c8c4861f1df2b4bdb81ef0843840f7bd43
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-20 05:47:20 -0300

    :wrench: Contato: estilizando + funcionalidades

--- Client/src/screens/Contato/index.tsx
+++ Client/src/screens/Contato/index.tsx
@@ -1,9 +1,49 @@
 import './styles.css'
+import { IoIosSend } from 'react-icons/io'
+import { toast } from 'react-toastify'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function Contato() {
+  const [company, setCompany] = useState('')
+  const [name, setName] = useState('')
+  const [cellphone, setCellphone] = useState('')
+  const [email, setEmail] = useState('')
+  const [category, setCategory] = useState('')
+  const [address, setAddress] = useState('')
+
+  function handleSubmit() {
+    if (company === '' || name === '' || cellphone === '' || email === '' || category === '' || address === '') {
+      toast.error('Preencha todos os campos')
+      return
+    }
+    const newDate = new Date()
+    const date = newDate.getDate() + '/' + (newDate.getMonth()) + '/' + newDate.getFullYear()
+    axios.post("https://discord.com/api/webhooks/1021689784973926480/ZM-3vpkLj_AdQ1oh8uenlo-WZw--J3-B8HwatEz3WcWJu_353TRYYW-rWHnmFBxWejGf", {
+      content: "",
+      embeds: [
+        {
+          title: "Cadastrar nova empresa",
+          description: `**Empresa:** ${company}\n**Nome:** ${name}\n**Celular:** ${cellphone}\n**Email:** ${email}\n**Categoria:** ${category}\n**Endereço:** ${address}`,
+          footer: {
+            text: date,
+          }
+        }
+      ],
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    toast.success("Obrigado por entrar em contato conosco!")
+  }
+
   return (
     <section className="contato">
-      <h1>Entre em contato conosco</h1>
+      <div className="title-style-1">
+        <h1>Contato</h1>
+        <h2>Cadastre sua empresa também</h2>
+      </div>
       <div className="pagina">
         <div className="cartao">
           <h3>Endereço</h3>
@@ -12,60 +52,74 @@ export default function Contato() {
             src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEM-BKN-O6irIoDU8C-G9OFtPUvgb6bjg&q=-23°06'22.9, -50°22'18.7"
           ></iframe>
           <h3>Telefone</h3>
-          <p className="title">(43)3542-4525</p>
-          <p>Prefeitura Municipal de Bandeirantes</p>
-          <p>
-            <button className="hover:bg-gray-700 transition-colors hover:transition-colors">
-              Entrar em contato
-            </button>
-          </p>
+          <p className='mb-2'>Prefeitura Municipal de Bandeirantes</p>
+          <p>(43)3542-4525</p>
+
         </div>
         <div className="rigth">
           <div className="entreemcontato">
-            <h3>Envie-nos uma Mensagem </h3>
-            <form action="action_page.php">
-              <label htmlFor="nome">Nome:</label>
+            <h3>Cadastre aqui sua empresa</h3>
+            <form>
+              <label htmlFor="nome">Nome da empresa:</label>
               <input
                 type="text"
-                id="nome"
-                name="nome"
-                placeholder="Seu nome..."
+                id="empresa"
+                name="empresa"
+                placeholder="Digite o nome da empresa"
+                onChange={(event) => setCompany(event.target.value)}
               />
-              <label htmlFor="telefone">Telefone:</label>
-              <input
-                type="text"
-                id="Telefone"
-                name="telefone"
-                placeholder="Seu telefone..."
-              />
+              <div className='grid grid-cols-2 gap-4'>
+                <span>
+                  <label htmlFor="nome">Nome para contato:</label>
+                  <input
+                    type="text"
+                    id="nome"
+                    name="nome"
+                    placeholder="Digite seu nome para contato"
+                    onChange={(event) => setName(event.target.value)}
+                  />
+                </span>
+                <span>
+                  <label htmlFor="telefone">Telefone:</label>
+                  <input
+                    type="text"
+                    id="telefone"
+                    name="telefone"
+                    placeholder="Digite seu telefone"
+                    onChange={(event) => setCellphone(event.target.value)}
+                  />
+                </span>
+              </div>
               <label htmlFor="email">E-Mail:</label>
               <input
                 type="text"
                 id="email"
                 name="email"
-                placeholder="Seu e-mail..."
+                placeholder="Digite seu email"
+                onChange={(event) => setEmail(event.target.value)}
               />
-              <label htmlFor="tsugest">Tipo da sugestão:</label>
-              <select id="tsugest" name="tsugest">
+
+
+              <label htmlFor="tsugest">Categoria da empresa:</label>
+              <select id="tsugest" name="tsugest" onChange={(event) => setCategory(event.target.value)}>
                 <option value="turismo">Turismo</option>
                 <option value="restaurante">Restaurante</option>
-                <option value="hotéis">Hotéis</option>
-                <option value="agenda">Agenda</option>
+                <option value="bar">Bar</option>
+                <option value="hotel">Hotel</option>
                 <option value="outros">Outros...</option>
               </select>
-              <label htmlFor="sugestão">Sugestão:</label>
+              <label htmlFor="sugestão">Endereço completo:</label>
               <textarea
-                id="sugestão"
-                name="sugestão"
-                placeholder="Escreva aqui sua sujestão..."
-                style={{ height: '200px' }}
+                id="endereco"
+                name="endereco"
+                placeholder="Digite o endereço completo da empresa"
+                style={{ height: '100px' }}
                 defaultValue={''}
+                onChange={(event) => setAddress(event.target.value)}
               />
-              <input
-                type="enviar"
-                defaultValue="Enviar"
-                className="hover:bg-gray-700 transition-colors hover:transition-colors"
-              />
+              <button className="enviar" onClick={handleSubmit}>
+                <IoIosSend className='mr-2' size={20} /> Enviar
+              </button>
             </form>
           </div>
         </div>
