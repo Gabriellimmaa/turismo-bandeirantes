245accc783dbc67dc1eded6db92678e31f49a077
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-09-23 19:45:54 -0300

    :wheelchair: feat add traducao

--- Client/src/screens/Dashboard/CadastrarAgenda/index.tsx
+++ Client/src/screens/Dashboard/CadastrarAgenda/index.tsx
@@ -2,6 +2,9 @@ import { MdTitle } from 'react-icons/md';
 import { BiCategoryAlt } from 'react-icons/bi';
 import { BsTextIndentLeft } from 'react-icons/bs';
 import { BiMapPin } from 'react-icons/bi';
+
+import { useTranslation } from 'react-i18next';
+
 import {
     BiPlanet,
     BiPhoneCall,
@@ -12,17 +15,18 @@ import './styles.css'
 import marcadorTurismo from '../../../assets/marcadores/vermelho.png'
 
 export default function CadastrarAgenda() {
+    const { t } = useTranslation()
     return (
         <>
             <section className="cadastrar-turismo">
                 <div className="title-style-1">
-                    <h1>Cadastrar Agenda</h1>
+                    <h1>{t("paginas.dashboard.agenda.titulo")}</h1>
                     <h2></h2>
                 </div>
                 <form>
                     <div className='grid-cols-1 md:grid-cols-2'>
                         <span>
-                            <label htmlFor="titulo"><MdTitle /> Título: <span>*Obrigatório</span> </label>
+                            <label htmlFor="titulo"><MdTitle /> {t("paginas.dashboard.agenda.tituloTela")} <span>{t("paginas.dashboard.agenda.obrigatorio")}</span> </label>
                             <input
                                 type="text"
                                 id="titulo"
@@ -31,72 +35,72 @@ export default function CadastrarAgenda() {
                             />
                         </span>
                         <span>
-                            <label htmlFor="data"><BiCategoryAlt /> Data: <span>*Obrigatório</span></label>
+                            <label htmlFor="data"><BiCategoryAlt /> {t("paginas.dashboard.agenda.data")} <span>{t("paginas.dashboard.agenda.obrigatorio")}</span></label>
                             <input
                                 type="text"
                                 id="data"
                                 name="data"
-                                placeholder="Insira o data do evento"
+                                placeholder= {t("paginas.dashboard.agenda.placeHolderTelefone")}
                             />
                         </span>
                     </div>
 
                     <span>
-                        <label htmlFor="descricao"><BsTextIndentLeft /> Descrição: <span>*Obrigatório</span></label>
+                        <label htmlFor="descricao"><BsTextIndentLeft /> {t("paginas.dashboard.agenda.descricao")} <span>{t("paginas.dashboard.agenda.obrigatorio")}</span></label>
                         <textarea
                             id="descricao"
                             name="descricao"
-                            placeholder="Insira uma descrição do evento"
+                            placeholder= {t("paginas.dashboard.agenda.placeHolderDesc")}
                         />
                     </span>
 
                     <span>
-                        <label htmlFor="endereco"><BiMapPin /> Endereço: <span>*Obrigatório</span></label>
+                        <label htmlFor="endereco"><BiMapPin /> {t("paginas.dashboard.agenda.endereco")} <span>{t("paginas.dashboard.agenda.obrigatorio")}</span></label>
                         <input
                             type="text"
                             id="endereco"
                             name="endereco"
-                            placeholder="Insira o endereço do evento"
+                            placeholder= {t("paginas.dashboard.agenda.placeHolderEndereco")}
                         />
                     </span>
 
                     <div className='grid-cols-1 md:grid-cols-3'>
                         <span>
-                            <label htmlFor="email"><BiMailSend /> Email:</label>
+                            <label htmlFor="email"><BiMailSend /> {t("paginas.dashboard.agenda.email")}</label>
                             <input
                                 type="text"
                                 id="email"
                                 name="email"
-                                placeholder="Insira o email do evento caso tenha"
+                                placeholder= {t("paginas.dashboard.agenda.placeHolderEmail")}
                             />
                         </span>
                         <span>
-                            <label htmlFor="site"><BiPlanet /> Site:</label>
+                            <label htmlFor="site"><BiPlanet />{t("paginas.dashboard.agenda.site")}</label>
                             <input
                                 type="text"
                                 id="site"
                                 name="site"
-                                placeholder="Insira o site do evento caso tenha"
+                                placeholder= {t("paginas.dashboard.agenda.placeHolderSite")}
                             />
                         </span>
                         <span>
-                            <label htmlFor="telefone"><BiPhoneCall /> Telefone:</label>
+                            <label htmlFor="telefone"><BiPhoneCall /> {t("paginas.dashboard.agenda.telefone")}</label>
                             <input
                                 type="text"
                                 id="telefone"
                                 name="telefone"
-                                placeholder="Insira um telefone para contato"
+                                placeholder= {t("paginas.dashboard.agenda.placeHolderTelefone")}
                             />
                         </span>
                     </div>
 
                     <label htmlFor='file' style={{ display: 'block' }}>
-                        Escolha uma logo: <span>*Obrigatório</span>
+                    {t("paginas.dashboard.agenda.logo")}<span>{t("paginas.dashboard.agenda.obrigatorio")}</span>
                         <input type="file" id='file' name='file' className="w-full text-sm text-gray-500 bg-white file:mr-4 file:py-2 file:px-4 file:border-0 file:bg-gray-200 hover:file:bg-gray-400 file:cursor-pointer" />
                     </label>
 
                     <button>
-                        Cadastrar Evento 
+                    {t("paginas.dashboard.agenda.cadastro")}
                     </button>
                 </form>
             </section>
