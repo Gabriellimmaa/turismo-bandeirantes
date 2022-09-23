ed5d89a9717714f6a5805aab028f67fa4dbfffb3
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-23 19:52:35 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, '/build')))
+app.use(express.static(path.join(__dirname, '/build')))
 app.use(express.static('public'))
 app.use(bodyParser.json());
 
@@ -34,6 +34,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
-app.listen(3000, '0.0.0.0', function() {
-    console.log("Server started.......");
-  });
+app.listen(8000, () => console.log("Listening on port 8000"));
