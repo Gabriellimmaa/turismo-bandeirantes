142c4378bb38efbdf808e066b09cda5cfed49166
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-23 05:23:10 -0300

    :wrench: Filter option /restaurante /turismo

--- mobile/src/screens/Restaurantes.tsx
+++ mobile/src/screens/Restaurantes.tsx
@@ -1,23 +1,47 @@
-import { ScrollView, VStack } from "native-base";
-import { useEffect, useState } from "react";
+import { CheckIcon, Flex, ScrollView, Select, Text, VStack } from "native-base";
+import { useEffect, useRef, useState } from "react";
 import { CardPrincipal } from "../components/Card/Principal";
 import { Header } from "../components/Header";
 import { Loading } from "../components/Loading";
 import api from "../services/api";
 import { PropsGeral } from "../utils/tipagens";
+interface turismoProps {
+  id: number
+  nome: string
+  descricao: string
+  logo: string
+  preco: string
+  cardapio: string | null
+  cozinhas: string
+  telefone: string
+  latitude: string
+  longitude: string
+  categoria: string
+}
 
 export function Restaurantes() {
   const [restaurantes, setRestaurantes] = useState<PropsGeral[]>()
-  const [loading, setLoading] = useState(false)
+  const [bares, setBares] = useState<PropsGeral[]>()
+  const [loading1, setLoading1] = useState(false)
+  const [loading2, setLoading2] = useState(false)
+  const [filter, setFilter] = useState<string>('')
 
   useEffect(() => {
     api.get('/restaurantes').then((response) => {
-      setRestaurantes(response.data.restaurantes)
-      setLoading(true)
+      const result = response.data.restaurantes
+      setRestaurantes(result)
+      setLoading1(true)
+    })
+
+    api.get('/bares').then((response) => {
+      const result = response.data.bares
+      setBares(result)
+      setLoading2(true)
     })
   }, [])
 
-  if (!loading) {
+
+  if (!loading1 || !loading2) {
     return <Loading />
   }
 
@@ -25,26 +49,114 @@ export function Restaurantes() {
     <VStack flex={1} mb={10} bg="gray.100" >
       <Header />
       <ScrollView pt={4} >
-        { restaurantes!.map((item) => (
-          <CardPrincipal
-            key={item.id}
-            id={item.id}
-            descricao={item.descricao}
-            email={item.email}
-            endereco={item.endereco}
-            face={item.face}
-            insta={item.insta}
-            latitude={item.latitude}
-            logo={item.logo}
-            longitude={item.longitude}
-            nome={item.nome}
-            preco={item.preco}
-            site={item.site}
-            telefone={item.telefone}
-            whats={item.whats}
-            tipo="Restaurante"
-          />
-        )) }
+        <Flex direction="row" alignItems="center" justifyContent="center" w="100%" mb={3}>
+          <Text>Odernar por:</Text>
+          <Select mx={4} selectedValue={filter} minWidth={200} accessibilityLabel="Selecione uma opção" placeholder="Selecione uma opção" _selectedItem={{
+            bg: "teal.600",
+            endIcon: <CheckIcon size="5" />
+          }} mt={1} onValueChange={itemValue => setFilter(itemValue)}>
+            <Select.Item label="Tudo" value="all" />
+            <Select.Item label="Restaurante" value="restaurante" />
+            <Select.Item label="Lanchonete" value="lanchonete" />
+            <Select.Item label="Bar" value="bar" />
+            <Select.Item label="Pizzaria" value="pizzaria" />
+            <Select.Item label="Padaria" value="padaria" />
+          </Select>
+        </Flex>
+
+        {restaurantes!.map((item) => {
+          if (filter === 'all' || filter === '') {
+            return (
+              <CardPrincipal
+                key={item.id}
+                id={item.id}
+                descricao={item.descricao}
+                email={item.email}
+                endereco={item.endereco}
+                face={item.face}
+                insta={item.insta}
+                latitude={item.latitude}
+                logo={item.logo}
+                longitude={item.longitude}
+                nome={item.nome}
+                preco={item.preco}
+                site={item.site}
+                telefone={item.telefone}
+                whats={item.whats}
+                tipo="Restaurante"
+              />
+            )
+          } else if (filter === "restaurante") {
+            return (
+              <CardPrincipal
+                key={item.id}
+                id={item.id}
+                descricao={item.descricao}
+                email={item.email}
+                endereco={item.endereco}
+                face={item.face}
+                insta={item.insta}
+                latitude={item.latitude}
+                logo={item.logo}
+                longitude={item.longitude}
+                nome={item.nome}
+                preco={item.preco}
+                site={item.site}
+                telefone={item.telefone}
+                whats={item.whats}
+                tipo="Restaurante"
+              />
+            )
+          }
+          return null
+        })}
+
+        {bares!.map((item) => {
+          if (filter === 'all' || filter === '') {
+            return (
+              <CardPrincipal
+                key={item.id}
+                id={item.id}
+                descricao={item.descricao}
+                email={item.email}
+                endereco={item.endereco}
+                face={item.face}
+                insta={item.insta}
+                latitude={item.latitude}
+                logo={item.logo}
+                longitude={item.longitude}
+                nome={item.nome}
+                preco={item.preco}
+                site={item.site}
+                telefone={item.telefone}
+                whats={item.whats}
+                tipo="Bar"
+              />
+            )
+          } else if (filter === "bar") {
+            return (
+              <CardPrincipal
+                key={item.id}
+                id={item.id}
+                descricao={item.descricao}
+                email={item.email}
+                endereco={item.endereco}
+                face={item.face}
+                insta={item.insta}
+                latitude={item.latitude}
+                logo={item.logo}
+                longitude={item.longitude}
+                nome={item.nome}
+                preco={item.preco}
+                site={item.site}
+                telefone={item.telefone}
+                whats={item.whats}
+                tipo="Bar"
+              />
+            )
+          }
+          return null
+        })}
       </ScrollView>
     </VStack>
   )
--- mobile/src/screens/Turismo.tsx
+++ mobile/src/screens/Turismo.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, VStack } from "native-base";
+import { Box, Center, CheckIcon, Flex, ScrollView, Select, Text, VStack } from "native-base";
 import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 import { Loading } from "../components/Loading";
@@ -12,6 +12,8 @@ export function Turismo() {
   const [religioso, setReligioso] = useState(true)
   const [cultural, setCultural] = useState(true)
   const [ecologico, setEcologico] = useState(true)
+  const [filter, setFilter] = useState<string>('')
+
 
   useEffect(() => {
     api.get('/atracoes').then((response) => {
@@ -30,28 +32,72 @@ export function Turismo() {
   return (
     <VStack flex={1} mb={10} bg="gray.100" >
       <Header />
+
       <ScrollView pt={4} >
-        { turismo.map((item) => (
-          <CardPrincipal
-            key={item.id}
-            id={item.id}
-            descricao={item.descricao}
-            email={item.email}
-            endereco={item.endereco}
-            face={item.face}
-            insta={item.insta}
-            latitude={item.latitude}
-            logo={item.logo}
-            longitude={item.longitude}
-            nome={item.nome}
-            preco={item.preco}
-            site={item.site}
-            telefone={item.telefone}
-            whats={item.whats}
-            categoria={item.categoria}
-            tipo="Turismo"
-          />
-        )) }
+        
+        <Flex direction="row" alignItems="center" justifyContent="center" w="100%" mb={3}>
+          <Text>Odernar por:</Text>
+          <Select mx={4} selectedValue={filter} minWidth={200} accessibilityLabel="Selecione uma opção" placeholder="Selecione uma opção" _selectedItem={{
+            bg: "teal.600",
+            endIcon: <CheckIcon size="5" />
+          }} mt={1} onValueChange={itemValue => setFilter(itemValue)}>
+            <Select.Item label="Tudo" value="all" />
+            <Select.Item label="Turismo Religioso" value="Religioso" />
+            <Select.Item label="Turismo Cultural" value="Cultural" />
+            <Select.Item label="Turismo Rural" value="Rural" />
+            <Select.Item label="Turismo Esportivo" value="Esportivo" />
+            <Select.Item label="Turismo Ecológico" value="Ecológico" />
+          </Select>
+        </Flex>
+
+        {turismo.map((item) => {
+          if (filter === 'all' || filter === '') {
+            return (
+              <CardPrincipal
+                key={item.id}
+                id={item.id}
+                descricao={item.descricao}
+                email={item.email}
+                endereco={item.endereco}
+                face={item.face}
+                insta={item.insta}
+                latitude={item.latitude}
+                logo={item.logo}
+                longitude={item.longitude}
+                nome={item.nome}
+                preco={item.preco}
+                site={item.site}
+                telefone={item.telefone}
+                whats={item.whats}
+                categoria={item.categoria}
+                tipo="Turismo"
+              />
+            )
+          } else if (item.categoria === filter) {
+            return (
+              <CardPrincipal
+                key={item.id}
+                id={item.id}
+                descricao={item.descricao}
+                email={item.email}
+                endereco={item.endereco}
+                face={item.face}
+                insta={item.insta}
+                latitude={item.latitude}
+                logo={item.logo}
+                longitude={item.longitude}
+                nome={item.nome}
+                preco={item.preco}
+                site={item.site}
+                telefone={item.telefone}
+                whats={item.whats}
+                categoria={item.categoria}
+                tipo="Turismo"
+              />
+            )
+          }
+          return null
+        })}
       </ScrollView>
     </VStack>
   );
