1a3b6341335d9798613fb97b7087ee8ea59721d9
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-23 20:00:36 -0300

    Revert "teste"
    This reverts commit aa5743cb1009c073a5f17a8058801257872107fc.

---
+++ src/build/precache-manifest.34359d0deeffec160f85a0877cb4dafd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01e2d15c360f6df93f5d",
+    "url": "/static/css/main.49a4b1fd.chunk.css"
+  },
+  {
+    "revision": "01e2d15c360f6df93f5d",
+    "url": "/static/js/main.d62f11fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d48828b1eee0aee7aab",
+    "url": "/static/js/2.45ee8137.chunk.js"
+  },
+  {
+    "revision": "b4b9c44c09ccd501c8fc0c6f42134f62",
+    "url": "/index.html"
+  }
+];
