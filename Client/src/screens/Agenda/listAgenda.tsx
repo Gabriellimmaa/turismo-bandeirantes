74799e71237e5ba01ceede5aedc773688d8c3b70
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-09-24 12:23:11 -0300

    :construction: refactor: Organizando dados com lint

--- Client/src/screens/Agenda/listAgenda.tsx
+++ Client/src/screens/Agenda/listAgenda.tsx
@@ -1,35 +1,41 @@
 export const listAgenda = [
-    {
-        id: '01',
-        titulo: 'Aniversário de Bandeirantes',
-        descricao: 'Neste dia 14 de novembro, Bandeirantes completa 87 anos. Nesta ocasião, quero parabenizar o povo de Bandeirantes, que é gente de bem, que é guerreira, que é honesta e trabalhadora. Nossa população é solidária, é amiga, é humana, é batalhadora e não mede esforços para juntos caminharmos e construirmos o desenvolvimento do nosso Município.',
-        data: '14/11/2022',
-        endereco: '',
-        website: '',
-        telefone: '',
-        email: '',
-        imagem: 'https://cdn.discordapp.com/attachments/792256194932375563/1022411969367658607/unknown.png',
-    },
-    {
-        id: '02',
-        titulo: 'Natal comunitário',
-        descricao: 'O Natal é uma das mais importantes datas festivas do cristianismo, sendo celebrado anualmente no dia 25 de dezembro. Vamos dar uma pequena festa e distribulir alegria para todos os públicos.',
-        data: '25/12/2022',
-        endereco: 'Santuário Santa Terezinha do Menino Jesus e da Sagrada Face',
-        website: '',
-        telefone: '',
-        email: '',
-        imagem: 'https://cdn.discordapp.com/attachments/792256194932375563/1022412164448923668/unknown.png',
-    },
-    {
-        id: '03',
-        titulo: 'Novena de São Miguel Arcanjo',
-        descricao: 'Horário das Missas com a Novena: 16h e 19h30 (de segunda a sábado). Domingo dia 25: Missa às 6h, 9h, 12h, 16h às 19h30. Haverá também bênção dos carros às 15h.',
-        data: '20/09/2022 a 28/09/2022',
-        endereco: 'BR-369, KM58 - Bandeirantes, PR, 86360-000',
-        website: '',
-        telefone: '(43) 3145-7500',
-        email: '',
-        imagem: 'https://cdn.discordapp.com/attachments/792256194932375563/1022411934647201792/unknown.png',
-    }
+  {
+    id: '01',
+    titulo: 'Aniversário de Bandeirantes',
+    descricao:
+      'Neste dia 14 de novembro, Bandeirantes completa 87 anos. Nesta ocasião, quero parabenizar o povo de Bandeirantes, que é gente de bem, que é guerreira, que é honesta e trabalhadora. Nossa população é solidária, é amiga, é humana, é batalhadora e não mede esforços para juntos caminharmos e construirmos o desenvolvimento do nosso Município.',
+    data: '14/11/2022',
+    endereco: '',
+    website: '',
+    telefone: '',
+    email: '',
+    imagem:
+      'https://cdn.discordapp.com/attachments/792256194932375563/1022411969367658607/unknown.png',
+  },
+  {
+    id: '02',
+    titulo: 'Natal comunitário',
+    descricao:
+      'O Natal é uma das mais importantes datas festivas do cristianismo, sendo celebrado anualmente no dia 25 de dezembro. Vamos dar uma pequena festa e distribulir alegria para todos os públicos.',
+    data: '25/12/2022',
+    endereco: 'Santuário Santa Terezinha do Menino Jesus e da Sagrada Face',
+    website: '',
+    telefone: '',
+    email: '',
+    imagem:
+      'https://cdn.discordapp.com/attachments/792256194932375563/1022412164448923668/unknown.png',
+  },
+  {
+    id: '03',
+    titulo: 'Novena de São Miguel Arcanjo',
+    descricao:
+      'Horário das Missas com a Novena: 16h e 19h30 (de segunda a sábado). Domingo dia 25: Missa às 6h, 9h, 12h, 16h às 19h30. Haverá também bênção dos carros às 15h.',
+    data: '20/09/2022 a 28/09/2022',
+    endereco: 'BR-369, KM58 - Bandeirantes, PR, 86360-000',
+    website: '',
+    telefone: '(43) 3145-7500',
+    email: '',
+    imagem:
+      'https://cdn.discordapp.com/attachments/792256194932375563/1022411934647201792/unknown.png',
+  },
 ]
