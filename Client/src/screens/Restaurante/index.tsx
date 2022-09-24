31df7d19b2c680b03e143fa3621986a6994b2f28
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-09-24 12:30:55 -0300

    :construction: refactor: código com lint

--- Client/src/screens/Restaurante/index.tsx
+++ Client/src/screens/Restaurante/index.tsx
@@ -5,18 +5,7 @@ import { optionRestaurante } from './optionData'
 import { useEffect, useState } from 'react'
 import api from '../../services/api'
 import { Loading } from '../../components/Loading'
-import ContentLoader, { Facebook } from 'react-content-loader'
-import { MdSettingsBackupRestore } from 'react-icons/md'
 import { useTranslation } from 'react-i18next'
-const MyLoader = () => (
-  <ContentLoader viewBox="0 0 380 70">
-    {/* Only SVG shapes */}
-    <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
-    <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-    <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-  </ContentLoader>
-)
-const MyFacebookLoader = () => <Facebook />
 
 interface turismoProps {
   id: number
@@ -38,7 +27,6 @@ export default function Restaurantes() {
   const [loading, setLoading] = useState(true)
   const [filter, setFilter] = useState<string>('')
   const { t } = useTranslation()
-
 
   useEffect(() => {
     api.get('/restaurantes').then((response) => {
@@ -145,24 +133,3 @@ export default function Restaurantes() {
     </section>
   )
 }
-
-/**
- * {bares.map((data) => {
-          return (
-            <Card
-              key={data.id}
-              title={data.nome}
-              description={data.descricao}
-              cell={data.telefone}
-              price={data?.preco ? data.preco : ''}
-              kitchen={data?.cozinhas ? data.cozinhas : ''}
-              menu={data?.cardapio ? data.cardapio : ''}
-              latitude={data.latitude}
-              longitude={data.longitude}
-              img={data.logo}
-              id={data.id}
-              type="restaurante"
-            />
-          )
-        })}
- */
