9d9ff6768204578d46e1d0d9f5bd044f0bf379f1
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-09-26 19:25:00 -0300

    :lipstick: feat: style lint

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -6,8 +6,6 @@ import './i18next'
 import { ToastContainer } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css'
-// minified version is also included
-// import 'react-toastify/dist/ReactToastify.min.css';
 
 import ScrollToTop from './hooks/scrollToTop'
 
--- Client/src/hooks/scrollToTop.tsx
+++ Client/src/hooks/scrollToTop.tsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function ScrollToTop() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation()
 
-    useEffect(() => {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth"
-        });
-    }, [pathname]);
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    })
+  }, [pathname])
 
-    return null;
+  return null
 }
